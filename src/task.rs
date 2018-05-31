e53997412a33479cf852a3e2ca0aa7682caed4fd
Ryan P. Brewster <RyanPBrewster@gmail.com>
2018-05-30 23:41:12 -0700

    Awful actor system garbage

--- src/task.rs
+++ src/task.rs
@@ -1,3 +1,6 @@
+use actix::*;
+use futures::future::Future;
+
 use std::collections::BTreeMap;
 use std::path::PathBuf;
 use std::process::Command;
@@ -22,18 +25,28 @@ impl Task {
     }
 
     pub fn run(&mut self) {
+        let system = System::new("task-queue");
+        let addr: Addr<Unsync, _> = TaskActor.start();
         for (input, state) in self.inputs.iter_mut() {
+            println!("marking {} as started", input);
             *state = TaskState::STARTED;
-            let result = Command::new(&self.command[0])
-                .args(&self.command[1..])
-                .arg(input)
-                .spawn();
-            *state = if result.is_ok() {
-                TaskState::SUCCEEDED
-            } else {
-                TaskState::FAILED
-            };
+            let cmd_req = CommandRequest(self.command.clone(), input.clone());
+            let request = addr.send(cmd_req);
+            Arbiter::handle().spawn(
+                request
+                    .map(|result| {
+                        println!("result = {}", result);
+                        if result {
+                            println!("marking as done");
+                        } else {
+                            println!("marking as failed");
+                        };
+                    })
+                    .map_err(|_| ()),
+            );
         }
+
+        system.run();
     }
 }
 
@@ -43,4 +56,26 @@ enum TaskState {
     STARTED,
     FAILED,
     SUCCEEDED,
+}
+
+struct TaskActor;
+impl Actor for TaskActor {
+    type Context = Context<Self>;
+}
+
+struct CommandRequest(Vec<String>, String);
+impl Message for CommandRequest {
+    type Result = bool;
+}
+impl Handler<CommandRequest> for TaskActor {
+    type Result = bool;
+
+    fn handle(&mut self, msg: CommandRequest, ctx: &mut Context<Self>) -> Self::Result {
+        println!("actually running {}", msg.1);
+        let result = Command::new(&(msg.0)[0])
+            .args(&(msg.0)[1..])
+            .arg(msg.1)
+            .spawn();
+        result.is_ok()
+    }
 }
