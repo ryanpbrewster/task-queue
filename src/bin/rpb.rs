ff359a4afa1e31cb73dd67dae3ba98b70e459e02
Ryan P. Brewster <RyanPBrewster@gmail.com>
2018-06-03 22:51:32 -0700

    Introduce show

--- src/bin/rpb.rs
+++ src/bin/rpb.rs
@@ -33,6 +33,9 @@ enum Operation {
 
     #[structopt(name = "run")]
     Run { task_id: i64 },
+
+    #[structopt(name = "show")]
+    Show { task_id: i64 },
 }
 
 fn main() {
@@ -74,20 +77,30 @@ fn main() {
             }
         }
         Operation::Delete { task_ids } => {
-            for id in task_ids {
-                conn.execute("DELETE FROM tasks WHERE id = ?1", &[&id])
+            for task_id in task_ids {
+                conn.execute("DELETE FROM tasks WHERE id = ?1", &[&task_id])
                     .expect("delete row from `tasks`");
+                conn.execute("DELETE FROM inputs WHERE task_id = ?1", &[&task_id])
+                    .expect("delete rows from `inputs`");
             }
         }
         Operation::List => {
-            let mut statement = conn.prepare("SELECT id, create_time, command FROM tasks")
-                .expect("prepare query for `tasks`");
+            let mut statement = conn.prepare(
+                "
+                 SELECT id, create_time, command,
+                 (SELECT COUNT(1) FROM inputs WHERE task_id = tasks.id)
+                 FROM tasks",
+            ).expect("prepare query for `tasks`");
             let mut query = statement.query(&[]).expect("perform query on `tasks`");
             while let Some(Ok(row)) = query.next() {
                 let id: i64 = row.get(0);
                 let create_time: DateTime<Utc> = row.get(1);
                 let command: String = row.get(2);
-                println!("[{}] {} --- {}", id, create_time, command);
+                let input_count: u32 = row.get(3);
+                println!(
+                    "[{}] {} --- {} inputs --- {}",
+                    id, create_time, input_count, command
+                );
             }
         }
         Operation::Run { task_id } => {
@@ -114,6 +127,31 @@ fn main() {
                     .arg(&input)
                     .spawn()
                     .expect("run command");
+            }
+        }
+        Operation::Show { task_id } => {
+            let mut command: String = conn.query_row(
+                "SELECT command FROM tasks WHERE id = ?1",
+                &[&task_id],
+                |row| row.get(0),
+            ).expect("query row");
+
+            println!("{}", command);
+
+            let mut statement = conn.prepare("SELECT id, value FROM inputs WHERE task_id = ?1")
+                .expect("prepare query for `inputs`");
+            let mut query = statement
+                .query_map(&[&task_id], |row| {
+                    let input_id: i64 = row.get(0);
+                    let value: String = row.get(1);
+                    (input_id, value)
+                })
+                .expect("perform query on `inputs`");
+
+            let inputs: BTreeMap<i64, String> =
+                query.collect::<Result<_, _>>().expect("aggregate inputs");
+            for (_, input) in inputs.into_iter() {
+                println!("  - {}", input);
             }
         }
     };
