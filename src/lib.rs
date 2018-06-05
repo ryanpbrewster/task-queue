8072dd64570a72e6838ec9d8e5d74fb68ebba2ea
Ryan P. Brewster <RyanPBrewster@gmail.com>
2018-06-04 22:30:33 -0700

    Track input state

--- src/lib.rs
+++ src/lib.rs
@@ -2,12 +2,64 @@ extern crate chrono;
 extern crate rusqlite;
 
 use chrono::{DateTime, Utc};
+use rusqlite::types::{FromSql, FromSqlError, ToSql, ToSqlOutput, ValueRef};
 use rusqlite::{Connection, Error};
 use std::collections::BTreeMap;
 use std::process::Command;
 
 pub struct TaskQueue {
     conn: Connection,
+}
+
+#[derive(Debug)]
+enum InputState {
+    New,
+    Started(DateTime<Utc>),
+    Finished(DateTime<Utc>),
+    Failed(DateTime<Utc>),
+}
+
+impl InputState {
+    pub fn char(&self) -> char {
+        match *self {
+            InputState::New => ' ',
+            InputState::Started(_) => '.',
+            InputState::Failed(_) => 'X',
+            InputState::Finished(_) => 'o',
+        }
+    }
+}
+
+impl ToSql for InputState {
+    fn to_sql(&self) -> Result<ToSqlOutput, Error> {
+        Ok(ToSqlOutput::Borrowed(ValueRef::Text(match *self {
+            InputState::New => "NEW",
+            InputState::Started(_) => "STARTED",
+            InputState::Finished(_) => "FINISHED",
+            InputState::Failed(_) => "FAILED",
+        })))
+    }
+}
+
+impl FromSql for InputState {
+    fn column_result(value: ValueRef) -> Result<Self, FromSqlError> {
+        match value {
+            ValueRef::Text(ref txt) => {
+                if txt.starts_with("NEW") {
+                    Ok(InputState::New)
+                } else if txt.starts_with("STARTED") {
+                    Ok(InputState::Started(Utc::now()))
+                } else if txt.starts_with("FAILED") {
+                    Ok(InputState::Failed(Utc::now()))
+                } else if txt.starts_with("FINISHED") {
+                    Ok(InputState::Finished(Utc::now()))
+                } else {
+                    Err(FromSqlError::InvalidType)
+                }
+            }
+            _ => Err(FromSqlError::InvalidType),
+        }
+    }
 }
 
 impl TaskQueue {
@@ -27,9 +79,7 @@ impl TaskQueue {
                     id              INTEGER PRIMARY KEY,
                     task_id         INTEGER,
                     value           TEXT NOT NULL,
-                    start_time      TEXT,
-                    finish_time     TEXT,
-                    error_time      TEXT
+                    state           TEXT NOT NULL
                 );",
         )?;
 
@@ -43,8 +93,8 @@ impl TaskQueue {
 
         for input in inputs {
             self.conn.execute(
-                "INSERT INTO INPUTS (task_id, value) VALUES (?1, ?2)",
-                &[&task_id, &input],
+                "INSERT INTO INPUTS (task_id, value, state) VALUES (?1, ?2, ?3)",
+                &[&task_id, &input, &InputState::New],
             )?;
         }
         Ok(())
@@ -86,20 +136,31 @@ impl TaskQueue {
             |row| row.get(0),
         )?;
 
-        let mut statement = self.conn
-            .prepare("SELECT id, value FROM inputs WHERE task_id = ?1")?;
-        let query = statement.query_map(&[&task_id], |row| {
-            let input_id: i64 = row.get(0);
-            let value: String = row.get(1);
-            (input_id, value)
-        })?;
+        let inputs: BTreeMap<i64, String> = {
+            let mut statement = self.conn
+                .prepare("SELECT id, value FROM inputs WHERE task_id = ?1")?;
+            let query = statement.query_map(&[&task_id], |row| {
+                let input_id: i64 = row.get(0);
+                let value: String = row.get(1);
+                (input_id, value)
+            })?;
 
-        let inputs: BTreeMap<i64, String> = query.collect::<Result<_, _>>()?;
-        for (_, input) in inputs.into_iter() {
-            Command::new(&command)
+            query.collect::<Result<_, _>>()?
+        };
+        for (input_id, input) in inputs.into_iter() {
+            self.set_input_state(input_id, InputState::Started(Utc::now()))?;
+            println!("starting: {} {}", command, input);
+            let state = if Command::new(&command)
                 .arg(&input)
-                .spawn()
-                .expect("actually run command");
+                .status()
+                .unwrap()
+                .success()
+            {
+                InputState::Finished(Utc::now())
+            } else {
+                InputState::Failed(Utc::now())
+            };
+            self.set_input_state(input_id, state)?;
         }
         Ok(())
     }
@@ -114,17 +175,27 @@ impl TaskQueue {
         println!("{}", command);
 
         let mut statement = self.conn
-            .prepare("SELECT id, value FROM inputs WHERE task_id = ?1")?;
-        let mut query = statement.query_map(&[&task_id], |row| {
+            .prepare("SELECT id, value, state FROM inputs WHERE task_id = ?1")?;
+        let query = statement.query_map(&[&task_id], |row| {
             let input_id: i64 = row.get(0);
             let value: String = row.get(1);
-            (input_id, value)
+            let state: InputState = row.get(2);
+            (input_id, value, state)
         })?;
 
-        let inputs: BTreeMap<i64, String> = query.collect::<Result<_, _>>()?;
-        for (_, input) in inputs.into_iter() {
-            println!("  - {}", input);
+        let inputs: Vec<(i64, String, InputState)> = query.collect::<Result<_, _>>()?;
+        for (_id, value, state) in inputs.into_iter() {
+            println!("  [{}] {}", state.char(), value);
         }
         Ok(())
+    }
+
+    fn set_input_state(&mut self, input_id: i64, state: InputState) -> Result<(), Error> {
+        self.conn
+            .execute(
+                "UPDATE inputs SET state = ?1 WHERE id = ?2 LIMIT 1",
+                &[&state, &input_id],
+            )
+            .map(|_| ())
     }
 }
